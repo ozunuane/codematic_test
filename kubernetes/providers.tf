0943517525bbad46ef33a6dbee19c10eb4c636f4
Ozimede Gp Unuane <godspower.u@9japay.com>
2024-08-26 22:33:34 +0100

    deploy infra to gcp

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -1,4 +1,4 @@
-  provider  "kubernetes" {
+provider "kubernetes" {
   host                   = "https://${module.gke-cluster.endpoint}"
   token                  = data.google_client_config.current.access_token
   cluster_ca_certificate = base64decode(module.gke-cluster.ca_certificate)
--- project/project.tf
+++ project/project.tf
@@ -6,9 +6,9 @@
 # }
 
 module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  source = "terraform-google-modules/project-factory/google//modules/project_services"
 
-  project_id  = var.gcp_project_id
+  project_id                  = var.gcp_project_id
   disable_services_on_destroy = false
   activate_apis = [
     "compute.googleapis.com",
